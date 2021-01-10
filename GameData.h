bc8e34083af322bf7cfe64bb27cb53e7b7998571
DarshK35 <73689839+DarshK35@users.noreply.github.com>
2021-01-10 17:03:16 +0530

    Code update
    Noticed that editor was indenting using spaces, so changed it to tabs

--- GameData.h
+++ GameData.h
@@ -6,240 +6,247 @@ enum suits { SPADE = 0, HEART, CLUB, DIAMOND };
 const int kalashnikov_hand[] = {13, 7, 4, 1};
 
 struct card {
-    int value, strength;
-    int suit;
+	int value, strength;
+	int suit;
 
-    void operator = (card& c) {
-        value = c.value;
-        strength = c.strength;
-        suit = c.suit;
-    }
+	void operator = (card& c) {
+		value = c.value;
+		strength = c.strength;
+		suit = c.suit;
+	}
 
-    bool operator == (card& c) {
-        int v = c.value;
-        int st = c.strength;
-        int su = c.suit;
-        if(v == value && st == strength && su == suit) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+	bool operator == (card& c) {
+		int v = c.value;
+		int st = c.strength;
+		int su = c.suit;
+		if(v == value && st == strength && su == suit) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 
-    void get_strength();
-    void disp();
+	void get_strength();
+	void disp();
 };
 
 void swap(card&, card&);
 
 class card_deck {
-    private:
-        card cards[52], shelf[16], garbage[36];
+	private:
+		card cards[52], shelf[16], garbage[36];
 
-        void init_cards();
-        void shuffle();
+		void init_cards();
+		void shuffle();
 
-    public:
-        card_deck();
-        int shelf_cards, garbage_cards, position;
+	public:
+		card_deck();
+		int shelf_cards, garbage_cards, position;
 
-        card pick_from_deck();
-        card pick_from_shelf(int);
-        void put_to_shelf(card);
-        void put_to_garbage(card);
+		void new_game();
+		card pick_from_deck();
+		card pick_from_shelf(int);
+		void put_to_shelf(card);
+		void put_to_garbage(card);
 
-        void debug_view();
+		void debug_view();
 
-        void view_shelf();
-        void view_garbage();
+		void view_shelf();
+		void view_garbage();
 };
 
 class player {
-    private:
-        card hand[4], picked;
-        int score;
-        void equate(card);
+	private:
+		card hand[4], picked;
+		int score;
+		void equate(card);
 
-    public:
-        player();
+	public:
+		player();
 
-        void shuffle_hand();
-        int calc_damage(int);
-        void add_score(int);
-        int ret_score();
-        bool kalashnikov();
+		void shuffle_hand();
+		int calc_damage(int);
+		void add_score(int);
+		int ret_score();
+		bool kalashnikov();
 
-        void pick_card(int);
-        void discard_card(int);
-        void put_card_shelf(int);
+		void pick_card(int);
+		void discard_card(int);
+		void put_card_shelf(int);
 
-        void start();
+		void start();
 
-        void view_hand();
-        void view_picked();
+		void view_hand();
+		void view_picked();
 };
 
 
+void card_deck::new_game() {
+	shuffle();
+	shelf_cards = 0;
+	garbage_cards = 0;
+	position = 0;
+}
 void card_deck::init_cards() {
-    for (int i = 0; i < 52; i++) {
-        cards[i].value = (i % 13) + 1;
-        cards[i].get_strength();
-    }
+	for (int i = 0; i < 52; i++) {
+		cards[i].value = (i % 13) + 1;
+		cards[i].get_strength();
+	}
 
-    int suit = SPADE;
-    for (int i = 1; i <= 52; i++) {
-        cards[i - 1].suit = suit;
-        switch (i) {
-            case 13:
-                suit = HEART;
-                break;
+	int suit = SPADE;
+	for (int i = 1; i <= 52; i++) {
+		cards[i - 1].suit = suit;
+		switch (i) {
+			case 13:
+				suit = HEART;
+				break;
 
-            case 26:
-                suit = CLUB;
-                break;
+			case 26:
+				suit = CLUB;
+				break;
 
-            case 39:
-                suit = DIAMOND;
-                break;
-        }
-    }
+			case 39:
+				suit = DIAMOND;
+				break;
+		}
+	}
 }
 void card_deck::shuffle() {
-    srand(time(NULL));
-    for (int i = 1; i < 52; i++)
-        swap(cards[i], cards[rand() % i]);
+	srand(time(NULL));
+	for (int i = 1; i < 52; i++)
+		swap(cards[i], cards[rand() % i]);
 }
 card_deck::card_deck() {
-    init_cards();
-    shuffle();
-    shelf_cards = 0;
-    garbage_cards = 0;
-    position = 0;
+	init_cards();
+	shuffle();
+	shelf_cards = 0;
+	garbage_cards = 0;
+	position = 0;
 }
 
 card card_deck::pick_from_deck() {
-    position++;
-    return cards[position - 1];
+	position++;
+	return cards[position - 1];
 }
 card card_deck::pick_from_shelf(int pos) {
-    card temp = shelf[pos];
-    for (int i = pos; i < shelf_cards - 1; i++)
-        shelf[i] = shelf[i + 1];
-    shelf_cards--;
-    return temp;
+	card temp = shelf[pos];
+	for (int i = pos; i < shelf_cards - 1; i++)
+		shelf[i] = shelf[i + 1];
+	shelf_cards--;
+	return temp;
 }
 void card_deck::put_to_garbage(card to_put) {
-    garbage[garbage_cards] = to_put;
-    garbage_cards++;
+	garbage[garbage_cards] = to_put;
+	garbage_cards++;
 }
 void card_deck::put_to_shelf(card to_put) {
-    shelf[shelf_cards] = to_put;
-    shelf_cards++;
+	shelf[shelf_cards] = to_put;
+	shelf_cards++;
 }
 
 player::player() {
-    score = 0;
+	score = 0;
 }
 void player::shuffle_hand() {
-    srand(time(NULL));
-    for (int i = 1; i < 4; i++)
-        swap(hand[i], hand[rand() % i]);
+	srand(time(NULL));
+	for (int i = 1; i < 4; i++)
+		swap(hand[i], hand[rand() % i]);
 }
 int player::calc_damage(int pos) {
-    card to_calc = hand[pos];
-    card *hand_copy = new card[4];
+	card to_calc = hand[pos];
+	card *hand_copy = new card[4];
 
-    //Creating sorted copy of the hand
-    for(int i = 0; i < 4; i++) {
-        hand_copy[i] = hand[i];
-    }
+	//Creating sorted copy of the hand
+	for(int i = 0; i < 4; i++) {
+		hand_copy[i] = hand[i];
+	}
 
-    int highest_val, p = 0;
-    for(int i = 0; i < 3; i++) {
-        highest_val = hand_copy[i].strength;
-        p = i;
-        for(int j = i + 1; j < 4; j++) {
-            if(highest_val < hand_copy[j].strength) {
-                highest_val = hand_copy[j].strength;
-                p = j;
-            }
-        }
-        swap(hand_copy[i], hand_copy[p]);
-    }
+	int highest_val, p = 0;
+	for(int i = 0; i < 3; i++) {
+		highest_val = hand_copy[i].strength;
+		p = i;
+		for(int j = i + 1; j < 4; j++) {
+			if(highest_val < hand_copy[j].strength) {
+				highest_val = hand_copy[j].strength;
+				p = j;
+			}
+		}
+		swap(hand_copy[i], hand_copy[p]);
+	}
 
-    //Calculating damage
-    int damage = 5;
-    for(int i = 0; i < 4; i++) {
-        if(hand[i - 1].strength != hand[i].strength && i > 0) {
-            damage--;
-        }
+	//Calculating damage
+	int damage = 5;
+	for(int i = 0; i < 4; i++) {
+		if(hand[i - 1].strength != hand[i].strength && i > 0) {
+			damage--;
+		}
 
-        if(hand[i] == to_calc) {
-            return damage;
-        }
-    }
+		if(hand[i] == to_calc) {
+			return damage;
+		}
+	}
 }
 void player::add_score(int increase) {
-    score += increase;
+	score += increase;
 }
 int player::ret_score() {
-    return score;
+	return score;
 }
 bool player::kalashnikov() {
-    card *copy = new card[4];
-    for(int i = 0; i < 4; i++) {
-        copy[i] = hand[i];
-    }
+	card *copy = new card[4];
+	for(int i = 0; i < 4; i++) {
+		copy[i] = hand[i];
+	}
 
-    int max, pos;
-    for(int i = 0; i < 3; i++) {
-        max = copy[i].value;
-        pos = i;
-        for(int j = i + 1; j < 4; j++) {
-            if(copy[j].value > max) {
-                max = copy[j].value;
-                pos = j;
-            }
-        }
-        swap(copy[i], copy[pos]);
-    }
+	int max, pos;
+	for(int i = 0; i < 3; i++) {
+		max = copy[i].value;
+		pos = i;
+		for(int j = i + 1; j < 4; j++) {
+			if(copy[j].value > max) {
+				max = copy[j].value;
+				pos = j;
+			}
+		}
+		swap(copy[i], copy[pos]);
+	}
 
-    for(int i = 0; i < 4; i++) {
-        if(copy[i].value != kalashnikov_hand[i])
-            return false;
-    }
-    return true;
+	for(int i = 0; i < 4; i++) {
+		if(copy[i].value != kalashnikov_hand[i])
+			return false;
+	}
+	return true;
 }
 
 void swap(card& a, card& b) {
-    card tem;
-    tem = a;
-    a = b;
-    b = tem;
+	card tem;
+	tem = a;
+	a = b;
+	b = tem;
 }
 void card::get_strength() {
-    switch(value) {
-        case 1:
-            strength = 13;
-            break;
+	switch(value) {
+		case 1:
+			strength = 13;
+			break;
 
-        case 13:
-            strength = 12;
-            break;
+		case 13:
+			strength = 12;
+			break;
 
-        case 12:
-            strength = 11;
-            break;
+		case 12:
+			strength = 11;
+			break;
 
-        case 11:
-            strength = 10;
-            break;
+		case 11:
+			strength = 10;
+			break;
 
-        default:
-            strength = value - 1;
-    }
+		default:
+			strength = value - 1;
+	}
 }
 void player::equate(card c) {
-    picked = c;
+	picked = c;
 }
--- Kalashnikov Card Game.cpp
+++ Kalashnikov Card Game.cpp
@@ -15,100 +15,108 @@ void player_turn();
 void comp_turn();
 
 int main() {
-    char e;
-    do {
-        e = menu();
-    } while(e != 'e');
+	char e;
+	do {
+		e = menu();
+	} while(e != 'e');
 }
 
 char menu() {
-    system("cls");
-    char ch, e;
+	system("cls");
+	char ch, e;
 
-    cout << "Kalashnikov - The Card Game";
-    cout << "Main Menu";
-    cout << "\nP -> Play Game";
-    cout << "\nI -> Instructions";
-    cout << "\nE -> Exit Game";
+	cout << "Kalashnikov - The Card Game";
+	cout << "Main Menu";
+	cout << "\nP -> Play Game";
+	cout << "\nI -> Instructions";
+	cout << "\nE -> Exit Game";
 
-    ch = _getch();
-    switch(ch){
-        case 'p':
-            game();
-            break;
+	ch = _getch();
+	switch(ch){
+		case 'p':
+			game();
+			break;
 
-        case 'i':
-            instructions();
-            break;
+		case 'i':
+			instructions();
+			break;
 
-        case 'e':
-            cout << "\n\nAre you sure? (Y / N)";
-            sure_exit:
-            e = _getch();
+		case 'e':
+			cout << "\n\nAre you sure? (Y / N)";
+			sure_exit:
+			e = _getch();
 
-            switch(e) {
-                case 'y':
-                    break;
+			switch(e) {
+				case 'y':
+					break;
 
-                case 'n':
-                    ch = e;
-                    break;
+				case 'n':
+					ch = e;
+					break;
 
-                default:
-                    goto sure_exit;
-            }
-    }
-    return ch;
+				default:
+					goto sure_exit;
+			}
+	}
+	return ch;
 }
 void game() {
-    p1.start();
-    comp.start();
-    do {
-        player_turn();
-        comp_turn();
-        break;
-    } while(p1.ret_score() < 20 && comp.ret_score() < 20);
+	int p1_score, ai_score;
+	bool over;
 
-    system("cls");
-    cout << "Game Over";
+	do {
+		p1_score = p1.ret_score();
+		ai_score = comp.ret_score();
+		over = false;
 
-    if(p1.ret_score() > comp.ret_score()) {
-        cout << "\n\nYou Win";
-    }
-    else {
-        cout << "\n\nYou Lose";
-    }
+		deck.new_game();
+		p1.start();
+		comp.start();
 
-    cout << "\n\nPress any key to return to menu";
-    _getch();
+
+
+	} while(p1.ret_score() < 20 && comp.ret_score() < 20);
+
+	system("cls");
+	cout << "Game Over";
+
+	if(p1_score > ai_score) {
+		cout << "\n\nYou Win";
+	}
+	else {
+		cout << "\n\nYou Lose";
+	}
+
+	cout << "\n\nPress any key to return to menu";
+	_getch();
 }
 void instructions() {
-    system("cls");
-    fstream file;
-    char ch;
+	system("cls");
+	fstream file;
+	char ch;
 
-    file.open("Instructions_screen.dat", ios::in);
-    while(!file.eof()) {
-        ch = file.get();
-        cout << ch;
-    }
-    _getch();
+	file.open("Instructions_screen.dat", ios::in);
+	while(!file.eof()) {
+	    ch = file.get();
+	    cout << ch;
+	}
+	_getch();
 }
 
 void player_turn() {
-    char ch;
-    system("cls");
-    cout << "\nYour hand:\n";
-    p1.view_hand();
+	char ch;
+	system("cls");
+	cout << "\nYour hand:\n";
+	p1.view_hand();
 
-    cout << "\n" << 52 - deck.position << " cards in deck";
+	cout << "\n" << 52 - deck.position << " cards in deck";
 
-    cout << "\n\nYour choices:";
-    if(p1.kalashnikov())
-        cout << "\nK -> KALASHNIKOV CYKA!";
-    if(52 - deck.position > 0)
-        cout << "\nD -> Pick card from deck";
-    //if(deck.shelf_cards)
+	cout << "\n\nYour choices:";
+	if(p1.kalashnikov())
+		cout << "\nK -> KALASHNIKOV CYKA!";
+	if(52 - deck.position > 0)
+		cout << "\nD -> Pick card from deck";
+	//if(deck.shelf_cards)
 }
 void comp_turn() {
 
@@ -118,88 +126,88 @@ void card_deck::view_shelf() {
 
 }
 void card_deck::debug_view() {
-    for(int i = 0; i < 52; i++) {
-        cards[i].disp();
-    }
+	for(int i = 0; i < 52; i++) {
+		cards[i].disp();
+	}
 }
 
 void player::start() {
-    for(int i = 0; i < 4; i++) {
-        pick_card(-1);
-        hand[i] = picked;
-    }
+	for(int i = 0; i < 4; i++) {
+		pick_card(-1);
+		hand[i] = picked;
+	}
 }
 void player::pick_card(int c) {
-    if(c == -1)
-        equate(deck.pick_from_deck());
-    else
-        equate(deck.pick_from_shelf(c));
+	if(c == -1)
+		equate(deck.pick_from_deck());
+	else
+		equate(deck.pick_from_shelf(c));
 }
 void player::discard_card(int pos) {
-    switch(pos) {
-        case 4:
-            deck.put_to_garbage(picked);
-            break;
-        default:
-            deck.put_to_garbage(hand[pos]);
-            hand[pos] = picked;
-    }
+	switch(pos) {
+		case 4:
+			deck.put_to_garbage(picked);
+			break;
+		default:
+			deck.put_to_garbage(hand[pos]);
+			hand[pos] = picked;
+	}
 }
 void player::put_card_shelf(int pos) {
-    switch(pos) {
-        case 4:
-            deck.put_to_shelf(picked);
-            break;
-        default:
-            deck.put_to_shelf(hand[pos]);
-            hand[pos] = picked;
-    }
+	switch(pos) {
+		case 4:
+			deck.put_to_shelf(picked);
+			break;
+		default:
+			deck.put_to_shelf(hand[pos]);
+			hand[pos] = picked;
+	}
 }
 void player::view_hand() {
-    for(int i = 0; i < 4; i++) {
-        hand[i].disp();
-    }
+	for(int i = 0; i < 4; i++) {
+		hand[i].disp();
+	}
 }
 void player::view_picked() {
-    picked.disp();
+	picked.disp();
 }
 
 void card::disp() {
-    switch(value) {
-        case 1:
-            cout << "A" << '\t';
-            break;
+	switch(value) {
+		case 1:
+			cout << "A" << '\t';
+			break;
+		
+		case 13:
+			cout << "K" << '\t';
+			break;
+			
+		case 12:
+			cout << "Q" << '\t';
+			break;
 
-        case 13:
-            cout << "K" << '\t';
-            break;
+		case 11:
+			cout << "J" << '\t';
+			break;
 
-        case 12:
-            cout << "Q" << '\t';
-            break;
+		default:
+			cout << value << '\t';
+	}
 
-        case 11:
-            cout << "J" << '\t';
-            break;
+	switch(suit) {
+		case SPADE:
+			cout << "Spade" << "\t\n";
+			break;
 
-        default:
-            cout << value << '\t';
-    }
+		case HEART:
+			cout << "Heart" << "\t\n";
+			break;
 
-    switch(suit) {
-        case SPADE:
-            cout << "Spade" << "\t\n";
-            break;
+		case CLUB:
+			cout << "Club" << "\t\n";
+			break;
 
-        case HEART:
-            cout << "Heart" << "\t\n";
-            break;
-
-        case CLUB:
-            cout << "Club" << "\t\n";
-            break;
-
-        case DIAMOND:
-            cout << "Diamond" << "\t\n";
-    }
+		case DIAMOND:
+			cout << "Diamond" << "\t\n";
+	}
 }
